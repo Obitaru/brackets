b52cf64845a4141b2f337080e1d85fec66e79682
Obitaru <f-frozen-93@mail.ru>
2023-01-22 20:40:12 +0400

    init: deploy completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0]) {
+        bracketsConfig[j][0] === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0] && stack.length > 0 ? stack.pop() : stack.push(str[i]);
+      } else {
+        if (str[i] == bracketsConfig[j][1]) {
+          if (stack[stack.length - 1] == bracketsConfig[j][0] && stack.length !== 0) {
+            stack.pop()
+          }
+        }
+      }
+    }
+  }
+  return stack.length === 0;
 }
